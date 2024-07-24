d4990626111c5457874a21fdc3b6d40147603b19
WIE HYANG HOON <gidgns1995@gmail.com>
2024-07-25 00:25:40 +0900

    [add] java date api

--- src/time/IsSupportedMain1.java
+++ src/time/IsSupportedMain1.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 
 public class IsSupportedMain1 {
     public static void main(String[] args) {
-        LocalDate now = LocalDate.now();
-        System.out.println("dt = " + dt);
-
-        if (now.isSupported())
+//        LocalDate now = LocalDate.now();
+//        System.out.println("dt = " + dt);
+//
+//        if (now.isSupported())
     }
 }
